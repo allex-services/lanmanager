3e8237d688942b5dc761bf5de2df17d9fdf9237a
Andrija Petrovic <andrija.hers@gmail.com>
2015-05-14 11:58:08 +0200

    Relying on allexcore StreamSubServiceExtractor now

--- consumers/basic.js
+++ consumers/basic.js
@@ -1,38 +1,14 @@
 function createConsumer(execlib){
   var lib = execlib.lib,
       execSuite = execlib.execSuite,
+      SubServiceExtractor = execSuite.StateSubServiceExtractor,
+      StateSource = execSuite.StateSource,
       ADS = execSuite.ADS,
+      StreamBlackHole = execSuite.StreamBlackHole,
       dataSuite = execlib.dataSuite,
       MemoryStorage = dataSuite.MemoryStorage,
       DataDecoder = dataSuite.DataDecoder;
 
-
-  function NeedsWaiter(consumer,sink){
-    this.consumer = consumer;
-    this.sink = sink;
-    this.waiter = ADS.listenToScalar(['haveneeds'],{rawsetter:this.goToNeeds.bind(this)});
-    sink.consumeChannel('s',this.waiter);
-  }
-  NeedsWaiter.prototype.destroy = function(){
-    this.waiter.destroy();
-    //this.sink.destroy();
-    this.sink = null;
-    this.consumer = null;
-  };
-  NeedsWaiter.prototype.goToNeeds = function(haveneeds){
-    console.log('Needs!',haveneeds);
-    this.sink.subConnect('needs',{name:'*'},{
-    }).done(
-      this.onNeedsSink.bind(this)
-    );
-  };
-  NeedsWaiter.prototype.onNeedsSink = function(nsink){
-    if(!nsink){
-      return;
-    }
-    this.consumer.takeNeedsSink(nsink);
-    this.destroy();
-  };
 
   function Consumer(){
     this.ready = new lib.HookCollection();
@@ -58,9 +34,12 @@ function createConsumer(execlib){
   };
   Consumer.prototype.takeSink = function(sink){
     console.log('lm user sink',sink.clientuser.client.identity.session);
-    new NeedsWaiter(this,sink);
+    //new NeedsWaiter(this,sink);
+    var initmap = new lib.Map;
+    initmap.add('needs',{identity:{role:'user'},propertyhash:{}});
+    sink.consumeChannel('s',StateSource.chain([new SubServiceExtractor(sink,initmap,this.takeSubSink.bind(this),true),new StreamBlackHole]));
   };
-  Consumer.prototype.takeNeedsSink = function(sink){
+  Consumer.prototype.takeSubSink = function(subname,sink){
     if(!this.needs){
       this.needs = new MemoryStorage({events:true,record:sink.recordDescriptor});
       this.needs.events.initiated.attach(this.ready.fire.bind(this.ready,this));
