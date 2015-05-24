2243b945a8f7236915445668549fda6f0bc74a2f
Andrija Petrovic <andrija.hers@gmail.com>
2015-05-24 13:42:34 +0200

    Cleanup and wrapup on the `user` User

--- methoddescriptors/user.js
+++ methoddescriptors/user.js
@@ -1,8 +1,4 @@
 module.exports = {
-  registerNewService:[{
-    title: 'Running Service descriptor',
-    type: 'object'
-  }],
   notifyServiceDown:[{
     title: 'Service instancename',
     type: 'string'
--- users/usercreator.js
+++ users/usercreator.js
@@ -10,43 +10,32 @@ function createUser(execlib,ParentUser){
 
   function User(prophash){
     ParentUser.call(this,prophash);
-    this.ip = prophash.ip;
-    console.log('new User',this.ip);
   }
   ParentUser.inherit(User,require('../methoddescriptors/user'),['haveneeds','haveservices']);
-  User.prototype.__cleanUp = function(){
+  User.prototype.destroy = function(){
     console.log('User',this.ip,'down, deleting the record');
     this.__service.subservices.get('services').call('delete',{
       op: 'eq',
       field: 'ipaddress',
-      value: this.ip
-    }).done(function(){
-      console.log('delete services done',arguments);
-    },function(){
-      console.error('delete services error',arguments);
-    });
-    ParentUser.prototype.__cleanUp.call(this);
-  };
-  User.prototype.registerNewService = function(runningservicedescriptor,defer){
-    console.log('registerNewService',runningservicedescriptor);
-    var needservice = this.__service.subservices.get('needs').subConnect(runningservicedescriptor.instancename,{name:this.get('name'),role:'user'},{}).done(
-      this.notifyNeedService.bind(this,defer,runningservicedescriptor),
-      defer.reject.bind(defer)
+      value: this.get('name')
+    }).done(
+      this.reallyDestroy.bind(this)
     );
   };
-  User.prototype.notifyNeedService = function(defer,runningservicedescriptor,needsink){
-    console.log('registerRunning',needsink.modulename,needsink.role);
-    needsink.call('registerRunning',runningservicedescriptor).done(
-      this.onNeedServiceNotified.bind(this,defer,runningservicedescriptor),
-      defer.reject.bind(defer)
-    );
-  };
-  User.prototype.onNeedServiceNotified = function(defer,runningservicedescriptor,result){
-    defer.resolve(result);
+  User.prototype.reallyDestroy = function(){
+    ParentUser.prototype.destroy.call(this);
   };
   User.prototype.notifyServiceDown = function(deadinstancename,defer){
     console.log(deadinstancename,'is dead');
-    defer.resolve('ok');
+    this.__service.subservices.get('services').call('delete',{
+      op: 'eq',
+      field: 'instancename',
+      value: deadinstancename
+    }).done(
+      defer.resolve.bind(defer),
+      defer.reject.bind(defer),
+      defer.notify.bind(defer)
+    );
   };
 
   return User;
